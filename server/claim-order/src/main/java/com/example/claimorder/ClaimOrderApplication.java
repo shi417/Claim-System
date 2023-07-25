3c80149f9d77be56710980145af5b25786ee94fd
shi417 <42686991+shi417@users.noreply.github.com>
2023-07-25 09:24:18 +0800

    建立工程

---
+++ server/claim-order/src/main/java/com/example/claimorder/ClaimOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.claimorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimOrderApplication.class, args);
+	}
+
+}
---
+++ server/claim-order/src/test/java/com/example/claimorder/ClaimOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.claimorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
